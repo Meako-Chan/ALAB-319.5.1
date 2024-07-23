3d99d63d082e27d0f4e675005e1bc8e7a92495d2
Michael Chan <michael.w.chan01@gmail.com>
2024-07-23 02:50:51 -0400

    Update route requests in grades.mjs

--- routes/grades.mjs
+++ routes/grades.mjs
@@ -8,7 +8,6 @@ const router = express.Router();
 
 // Create a single grade entry
 router.post("/", async (req, res) => {
-  // let collection = await db.collection("grades");
   try{
     let newDocument = req.body;
     if (newDocument.student_id) {
@@ -37,38 +36,48 @@ router.get("/:id", async (req, res) => {
 
 // Add a score to a grade entry
 router.patch("/:id/add", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { _id: ObjectId(req.params.id) };
+  try{
+    let query = { _id: ObjectId(req.params.id) };
 
-  let result = await collection.updateOne(query, {
-    $push: { scores: req.body }
-  });
+    let result = await Grade.updateOne(query, {
+      $push: { scores: req.body }
+    });
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+  } catch (error){
+    res.status(500).send(error.message);
+  }
 });
 
 // Remove a score from a grade entry
 router.patch("/:id/remove", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { _id: ObjectId(req.params.id) };
+  try{
+    let query = { _id: ObjectId(req.params.id) };
 
-  let result = await collection.updateOne(query, {
-    $pull: { scores: req.body }
-  });
+    let result = await Grade.updateOne(query, {
+      $pull: { scores: req.body }
+    });
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result) res.send("Not found").status(404);
+    res.send(result).status(200);
+  } catch(error) {
+    res.status(500).send(error.message);
+  }
+
 });
 
 // Delete a single grade entry
 router.delete("/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { _id: ObjectId(req.params.id) };
-  let result = await collection.deleteOne(query);
+  try{
+    let query = { _id: ObjectId(req.params.id) };
+    let result = await Grade.deleteOne(query);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result) res.send("Not found").status(404);
+    res.send(result).status(200);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 // Get route for backwards compatibility
@@ -78,65 +87,86 @@ router.get("/student/:id", async (req, res) => {
 
 // Get a learner's grade data
 router.get("/learner/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { learner_id: Number(req.params.id) };
+  try{
+    let query = { learner_id: Number(req.params.id) };
   
-  // Check for class_id parameter
-  if (req.query.class) query.class_id = Number(req.query.class);
+    // Check for class_id parameter
+    if (req.query.class) query.class_id = Number(req.query.class);
 
-  let result = await collection.find(query).toArray();
+    let result = await Grade.find(query);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result) res.send("Not found").status(404);
+    res.send(result).status(200);
+  } catch (error){
+    res.status(500).send(error.message);
+  }
+
 });
 
 // Delete a learner's grade data
 router.delete("/learner/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { learner_id: Number(req.params.id) };
+  try{
 
-  let result = await collection.deleteOne(query);
+    let query = { learner_id: Number(req.params.id) };
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    let result = await Grade.deleteMany(query);
+
+    if (!result) res.send("Not found").status(404);
+    res.send(result).status(200);
+  }catch (error){
+    res.status(500).send(error.message);
+  }
+
 });
 
 // Get a class's grade data
 router.get("/class/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { class_id: Number(req.params.id) };
+  try{
+    let query = { class_id: Number(req.params.id) };
 
-  // Check for learner_id parameter
-  if (req.query.learner) query.learner_id = Number(req.query.learner);
+    // Check for learner_id parameter
+    if (req.query.learner) query.learner_id = Number(req.query.learner);
 
-  let result = await collection.find(query).toArray();
+    let result = await Grade.find(query);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result) res.send("Not found").status(404);
+    res.send(result).status(200);
+  } catch (error){
+    res.status(500).send(error.message);
+  }
+
 });
 
 // Update a class id
 router.patch("/class/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { class_id: Number(req.params.id) };
+  try{
+    let query = { class_id: Number(req.params.id) };
 
-  let result = await collection.updateMany(query, {
-    $set: { class_id: req.body.class_id }
-  });
+    let result = await Grade.updateMany(query, {
+      $set: { class_id: req.body.class_id }
+    });
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result) res.send("Not found").status(404);
+    res.send(result).status(200);
+  } catch (error){
+    res.status(500).send(error.message);
+
+  }
 });
 
 // Delete a class
 router.delete("/class/:id", async (req, res) => {
-  let collection = await db.collection("grades");
-  let query = { class_id: Number(req.params.id) };
+  try{
+    let query = { class_id: Number(req.params.id) };
 
-  let result = await collection.deleteMany(query);
+    let result = await Grade.deleteMany(query);
 
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
+    if (!result) res.send("Not found").status(404);
+    res.send(result).status(200);
+  }catch (error){
+    res.status(500).send(error.message);
+  }
+
 });
 
 export default router;
